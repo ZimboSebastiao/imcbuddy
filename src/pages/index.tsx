f2400900e2b925ca97b101842a09694ea272d267
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-11-11 12:55:10 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -81,33 +81,30 @@ export default function Home() {
       </Script>
 
       <Script
-        type="text/javascript"
+        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
-          __html: `
-      (function(e, x, pe, r, i, me, nt) {
-        e[i] = e[i] || function() {
-          (e[i].a = e[i].a || []).push(arguments);
+          __html: `(function(e, x, pe, r, i, me, nt) {
+      e[i] = e[i] || function() {
+        (e[i].a = e[i].a || []).push(arguments);
+      };
+      me = x.createElement(pe);
+      me.async = 1;
+      me.src = r;
+      nt = x.getElementsByTagName(pe)[0];
+      me.addEventListener("error", function() {
+        function cb(t) {
+          t = t[t.length - 1];
+          if (typeof t === "function") t({ flags: {} });
+        }
+        if (Array.isArray(e[i].a)) e[i].a.forEach(cb);
+        e[i] = function() {
+          cb(arguments);
         };
-        me = x.createElement(pe);
-        me.async = 1;
-        me.src = r;
-        nt = x.getElementsByTagName(pe)[0];
-        me.addEventListener("error", function() {
-          function cb(t) {
-            t = t[t.length - 1];
-            if (typeof t === "function") t({ flags: {} });
-          }
-          if (Array.isArray(e[i].a)) e[i].a.forEach(cb);
-          e[i] = function() {
-            cb(arguments);
-          };
-        });
-        nt.parentNode.insertBefore(me, nt);
-      })(window, document, "script", "https://abt.s3.yandex.net/expjs/latest/exp.js", "ymab");
-
-      ymab("metrika.98887785", "setConfig", { enableSetYmUid: true });
-      ymab("metrika.98887785", "init");
-    `,
+      });
+      nt.parentNode.insertBefore(me, nt);
+    })(window, document, "script", "https://abt.s3.yandex.net/expjs/latest/exp.js", "ymab");
+    ymab("metrika.98887785", "setConfig", { enableSetYmUid: true });
+    ymab("metrika.98887785", "init");`,
         }}
       />
 
